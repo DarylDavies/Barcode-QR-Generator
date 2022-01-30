c233f65b230f044411430d65e325c03a18f054a0
Daryl Davies <puratturdaryl@gmail.com>
2022-01-30 22:56:51 +0530

    Add project files.

---
+++ QR - BarCode/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QR___BarCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using ZXing.QrCode;
+using ZXing;
+
+namespace QR___BarCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment)
+        {
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BarCode()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult BarCode(string barcode)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //The Image is drawn based on length of Barcode text.
+                using (Bitmap bitMap = new Bitmap(barcode.Length * 30, 80))
+                {
+                    //The Graphics library object is generated for the Image.
+                    using (Graphics graphics = Graphics.FromImage(bitMap))
+                    {
+                        //The installed Barcode font.
+                        Font oFont = new Font("IDAutomationHC39M Free Version", 16);
+                        PointF point = new PointF(2f, 2f);
+
+                        //White Brush is used to fill the Image with white color.
+                        SolidBrush whiteBrush = new SolidBrush(Color.White);
+                        graphics.FillRectangle(whiteBrush, 0, 0, bitMap.Width, bitMap.Height);
+
+                        //Black Brush is used to draw the Barcode over the Image.
+                        SolidBrush blackBrush = new SolidBrush(Color.Black);
+                        graphics.DrawString("*" + barcode + "*", oFont, blackBrush, point);
+                    }
+
+                    //The Bitmap is saved to Memory Stream.
+                    bitMap.Save(ms, ImageFormat.Png);
+
+                    //The Image is finally converted to Base64 string.
+                    ViewBag.BarcodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult QRCode()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult QRCode(IFormCollection formCollection)
+        {
+            var writer = new QRCodeWriter();
+            var resultBit = writer.encode(formCollection["QRCodeString"], BarcodeFormat.QR_CODE, 200, 200);
+            var matrix = resultBit;
+            int scale = 2;
+            Bitmap result = new Bitmap(matrix.Width * scale, matrix.Height * scale);
+            for (int x = 0; x < matrix.Height; x++)
+            {
+                for(int y = 0; y < matrix.Width; y++)
+                {
+                    Color pixel = matrix[x, y] ? Color.Black : Color.White;
+                    for (int i = 0; i < scale; i++)
+                        for (int j = 0; j < scale; j++)
+                            result.SetPixel(x * scale + i, y * scale + j, pixel);
+                }
+            }
+            string webRootPath = _hostEnvironment.WebRootPath;
+            result.Save(webRootPath + "\\Image\\QrcodeNew.png");
+            ViewBag.URL = "\\Image\\QrcodeNew.png";
+            return View();
+        }
+
+        public IActionResult ReadQRCode()
+        {
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var path = webRootPath + "\\Image\\QrcodeNew.png";
+            var reader = new BarcodeReaderGeneric();
+            Bitmap image = (Bitmap)Image.FromFile(path);
+            using (image)
+            {
+                LuminanceSource source;
+                source = new ZXing.Windows.Compatibility.BitmapLuminanceSource(image);
+                Result result = reader.Decode(source);
+                ViewBag.Text = result.Text;
+            }
+            return View("QRCode");
+        }
+    
+public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QR - BarCode/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+// Automatic Slideshow - change image every 3 seconds
+
+// Write your JavaScript code.
